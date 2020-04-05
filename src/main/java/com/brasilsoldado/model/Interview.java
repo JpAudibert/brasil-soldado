2c0e7cbc9a733cf158f7044583781ee4d941fba4
João Pedro Basso Audibert <hotsjoao@gmail.com>
2020-04-05 01:39:40 -0300

    Creating the models of the application

---
+++ src/main/java/com/brasilsoldado/model/Interview.java
@@ -0,0 +1,24 @@
+package com.brasilsoldado.model;
+
+public class Interview {
+
+    private int idInterview;
+    private String report;
+
+    public int getIdInterview() {
+        return idInterview;
+    }
+
+    public void setIdInterview(int idInterview) {
+        this.idInterview = idInterview;
+    }
+
+    public String getReport() {
+        return report;
+    }
+
+    public void setReport(String report) {
+        this.report = report;
+    }
+
+}
---
+++ src/main/java/com/brasilsoldado/model/Warning.java
@@ -0,0 +1,24 @@
+package com.brasilsoldado.model;
+
+public class Warning {
+
+    private int idWarning;
+    private String message;
+
+    public int getIdWarning() {
+        return idWarning;
+    }
+
+    public void setIdWarning(int idWarning) {
+        this.idWarning = idWarning;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
