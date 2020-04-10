ab02a909450a921e83609951556418d36c22ce6e
João Pedro Basso Audibert <hotsjoao@gmail.com>
2020-04-10 00:02:53 -0300

    starting to create the views

--- src/com/brasilsoldado/Main.java
+++ src/com/brasilsoldado/Main.java
@@ -1,22 +1,14 @@
 package com.brasilsoldado;
 
-import com.brasilsoldado.controller.CityController;
-import com.brasilsoldado.controller.LoginController;
-import com.brasilsoldado.controller.PersonController;
-import com.brasilsoldado.controller.StateController;
 import com.brasilsoldado.helpers.DBConnection;
-import com.brasilsoldado.model.City;
-import com.brasilsoldado.model.Person;
-import com.brasilsoldado.model.State;
-import java.sql.Date;
-import java.util.ArrayList;
+import com.brasilsoldado.view.ViewStart;
 
 public class Main {
 
     public static void main(String[] args) {
         if (DBConnection.getInstance() != null) {
             System.out.println("abriu");
-//            new FrmJanelaPrincipal().setVisible(true);
+            new ViewStart().setVisible(true);
         } else {
             System.out.println("deu problema");
         }
